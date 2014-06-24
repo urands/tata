90291b36eabded00501453dc79f18836436dd964
urands <ds@inbox.ru>
2014-06-24 12:00:42 +0400

    Update router

--- app/routes.php
+++ app/routes.php
@@ -10,14 +10,38 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
-
+/*
 Route::get('/', function()
 {
 	return View::make('hello');
 });
-
+*/
 
 Route::get('/test', function()
+{
+	return View::make('test');
+});
+
+
+Route::match(array('GET', 'POST'), '/true{id}/{id2}', function($id,$id2)
+{
+    return 'Hello World '.$id.'   '.$id2;
+});
+
+
+Route::any('foo', function()
+{
+    $id = 0;
+	$url = URL::to('me{id}');
+    return 'Hello World foo '.$url;
+});
+
+Route::get('user', array('before' => array('auth', 'old'), function()
+{
+    return 'You are authenticated and over 200 years old!';
+}));
+
+Route::get('/login', function()
 {
 	return View::make('test');
 });
